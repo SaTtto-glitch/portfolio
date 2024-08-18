02ee63c471f67f73335f3d4aaa8238c978909f71
SaTtto-glitch <saya.tomie@gmail.com>
2024-08-18 15:25:36 +0900

    ビルド修正2

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Top } from "./pages/top";
 import { BlogTop } from "./pages/blogs/blogTop";
--- libs/client.ts
+++ libs/client.ts
@@ -3,7 +3,7 @@ import { createClient } from "microcms-js-sdk";
 export type Blog = {
   id: string;
   title: string;
-  body: string;
+  contents: string;
 };
 
 if (!import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN) {
@@ -33,6 +33,5 @@ export const getDetail = async (contentId: string) => {
     endpoint: "blogs",
     contentId,
   });
-  console.log("Fetched blog:", blog);
   return blog;
 };
